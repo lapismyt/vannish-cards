79d48069e37fd97e49eca163a25786c83f6f5fcb
Nikita Ugnich <nikitagavrilin005@gmail.com>
2025-07-26 21:52:40 +0300

    add Creator and Hero

--- vannish_cards/randomizer.py
+++ vannish_cards/randomizer.py
@@ -2,7 +2,7 @@ import random
 from typing import TypeVar
 
 from .config import get_base_color, index
-from .data_types import Background, BaseColor, Rarity
+from .data_types import Background, BaseColor, PlayerRarity, Rarity
 from .render import RenderConfig
 
 KT = TypeVar("KT")
@@ -17,20 +17,21 @@ def choose_variant_with_probability(options: dict[KT, int]) -> KT:
 def random_render_config() -> RenderConfig:
     total_players = []
 
-    for _, player_nicknames in index["players"].items():
-        total_players.extend(player_nicknames)
+    # for _, player_nicknames in index["players"].items():
+    #     total_players.extend(player_nicknames)
 
     base_colors: dict[BaseColor, int] = index["chances"]["base_colors"]
-    # player_rarities: dict[PlayerRarity, int] = index["chances"]["players"]
+    player_rarities: dict[PlayerRarity, int] = index["chances"]["players"]
     backgrounds: dict[Background, int] = index["chances"]["backgrounds"]
     rarities: dict[Rarity, int] = index["chances"]["rarities"]
 
     base_color_name: BaseColor = choose_variant_with_probability(base_colors)
     base_color: str = get_base_color(base_color_name)
-    # player_rarity: PlayerRarity = choose_variant_with_probability(player_rarities)
+    player_rarity: PlayerRarity = choose_variant_with_probability(player_rarities)
     background: Background = choose_variant_with_probability(backgrounds)
     rarity: Rarity = choose_variant_with_probability(rarities)
-    player: str = random.choice(total_players)
+    # player: str = random.choice(total_players)
+    player: str = random.choice(index["players"][player_rarity])
 
     return RenderConfig(
         base_color=base_color,
